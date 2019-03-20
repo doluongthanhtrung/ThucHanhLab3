30869d0347b9f802568251b59b3bc5096bd5c667
Manh Ngo <ngomanh0405@gmail.com>
2019-03-20 09:32:35 +0700

    taoproject

---
+++ baibuoi3/baibuoi3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(baibuoi3.Startup))]
+namespace baibuoi3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
